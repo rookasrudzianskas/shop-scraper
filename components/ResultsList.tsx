86c17f49bba5dbf542f252d3fe8a395bd927d9bd
Rokas <rookas.rudzenskas@gmail.com>
2023-07-11 13:03:16 +0300

    FIX

--- components/ResultsList.tsx
+++ components/ResultsList.tsx
@@ -31,9 +31,9 @@ const ResultsList = ({results, term}: Props) => {
         ))}
       </div>
       {/* results */}
-      <div className="">
+      <div className="px-5 md:p-10 md:pt-0 space-y-5 flex-1">
         {results.map((pageResult, i) => (
-          <div key={pageResult.job_id} className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid">
+          <div key={pageResult.job_id} className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
             {i !== 0 && <hr className="w-full col-span-full" />}
 
             <div className="md:col-span-2 lg:col-span-3 xl:col-span-4 py-5">
@@ -48,16 +48,19 @@ const ResultsList = ({results, term}: Props) => {
             </div>
 
             {pageResult?.content?.results?.organic?.map((item, i) => (
-              <Link prefetch={false} href={item.url.includes("url?url=") ?
+              <Link className={`border rounded-2xl flex flex-col hover:shadow-lg transition duration-200 ease-in-out ${
+                item.url.includes('url?url=') && 'italic'
+              }`}
+                    prefetch={false} href={item.url.includes("url?url=") ?
                 item.url.split("url?url=")?.[1] :
                 item.url.split("?")?.[0]
               } key={item.pos}>
-                <div>
-                  <p>{item.title}</p>
+                <div className="border p-5 flex-1">
+                  <p className="text-[#1b66d2]">{item.title}</p>
                 </div>
-                <div>
-                  <p>{item.price_str} {item.currency}</p>
-                  <p>{item.merchant.name}</p>
+                <div className="px-5 py-2 not-italic">
+                  <p className="font-light">{item.price_str} {item.currency}</p>
+                  <p className="font-semibold text-[#1b66d2]">{item.merchant.name}</p>
                 </div>
               </Link>
             ))}
