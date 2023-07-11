1fc336460bf7db00cff55ae5dbb851cf7a7a8204
Rokas <rookas.rudzenskas@gmail.com>
2023-07-11 10:50:56 +0300

    FIX

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from "next/image";
 import Link from "next/link";
+import {MagnifyingGlassIcon} from "@heroicons/react/24/solid";
 
 const Header = ({}) => {
   return (
@@ -15,6 +16,16 @@ const Header = ({}) => {
           className="object-contain mr-10"
         />
       </Link>
+        <div>
+          <form>
+            <div>
+              <div className="flex items-center space-x-2 bg-white shadow-xl rounded-full border-0 px-6 py-4">
+                <MagnifyingGlassIcon className="w-5 h-5 text-gray-400" />
+                <input type="text" placeholder={'Search'} name="searchTerm" className="outline-none flex-1"/>
+              </div>
+            </div>
+          </form>
+        </div>
     </header>
   );
 };
