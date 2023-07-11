58deac31f0b415adaed2dc0c89fc7c575c538512
Rokas <rookas.rudzenskas@gmail.com>
2023-07-11 13:26:57 +0300

    FIX

--- app/search/[term]/loading.tsx
+++ app/search/[term]/loading.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const LoadingPage = ({}) => {
   return (
     <div>
-      Loading Page
+      <div>
+        <div className="w-36 md:w-64 space-y-5">
+          {[...Array(4)].map((_, i) => (
+            <div className="border rounded-r-lg md:rounded-lg p-5">
+              <h1 className="font-bold">
+                <Skeleton />
+              </h1>
+              <Skeleton count={(Math.random() * 5)  + 1} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
