1d93969733013eeb6d179dc2af8410cbacda408b
Rokas <rookas.rudzenskas@gmail.com>
2023-07-11 13:35:53 +0300

    FIX

--- app/search/[term]/loading.tsx
+++ app/search/[term]/loading.tsx
@@ -5,16 +5,33 @@ import 'react-loading-skeleton/dist/skeleton.css'
 const LoadingPage = ({}) => {
   return (
     <div>
-      <div>
+      <div className="flex md:px-5">
         <div className="w-36 md:w-64 space-y-5">
           {[...Array(4)].map((_, i) => (
             <div className="border rounded-r-lg md:rounded-lg p-5">
-              <h1 className="font-bold">
+              <p className="font-bold">
                 <Skeleton />
-              </h1>
+              </p>
               <Skeleton count={(Math.random() * 5)  + 1} />
             </div>
           ))}
+        </div>
+
+        <div className="px-5 md:p-10 py-0 space-y-5 flex-1">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
+            <div className="md:col-span-2 lg:col-span-3 xl:col-span-4 px-5 pb-2 text-xl font-semibold">
+              <h2>Loading Results from Google...</h2>
+              <h2 className="font-extralight text-base animate-pulse text-blue-500">Scraping Real Results Via Oxylab</h2>
+            </div>
+
+            {[...Array(10)].map((_, i) => (
+              <div className="p-5 bordr rounded-2xl">
+                <Skeleton count={2} />
+                <br/>
+                <Skeleton count={1} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
---
+++ app/shopping/product/[id]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const revalidate = 300;
+
+const ProductPage = ({}) => {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+};
+
+export default ProductPage;
+// by Rokas with ❤️
