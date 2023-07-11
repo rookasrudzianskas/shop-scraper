627c347889056d4d8d488ed3f9fd2177ebdc90e6
Rokas <rookas.rudzenskas@gmail.com>
2023-07-11 10:08:12 +0300

    FIX

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Image from "next/image";
 
 const Header = ({}) => {
   return (
     <header>
       <Image
         src="https://links.papareact.com/208"
+        alt={'Logo'}
+        width={150}
+        height={150}
       />
     </header>
   );
